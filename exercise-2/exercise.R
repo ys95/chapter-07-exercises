c37eae61078d95b7eaf06f35776da22f1e612270
Yihang <ys95@uw.edu>
2019-01-17 06:57:05 -0800

    add exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,47 +2,51 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- 10:20
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- seq(21, 30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
-
+all_numbers <- c(first_ten, next_ten)
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
-
+eleventh <- all_numbers[11]
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
-
+some_numbers <- all_numbers[2: 5]
 
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-
+tufa <- c(FALSE, TRUE)
+v <- c(1:100)
+even <- v[tufa]
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
-
+all(even, even %% 2 == 0)
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
-
+phone_numbers <- c(8, 6, 7, 5, 3, 0, 9)
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
-
+prefix <- phone_numbers[1:3]
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
-
+boo <- phone_numbers - 5 <= 0
+small <- phone_numbers[boo]
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
-
+boo <- phone_numbers - 5 > 0
+large <- phone_numbers[boo]
 
 # Replace the values in `phone_numbers` that are larger than 5 with the number 5
-
+inside <- c(1, 2, 3, 7)
+replace(phone_numbers, inside, 5)
 
 # Replace every odd-numbered value in `phone_numbers` with the number 0
-
+replace(phone_numbers, c(3, 4, 5, 7), 0)
